1353346f5f4c78427a1596061aad91b20df14db9
Angelo <157394877+AngeloRipamonti@users.noreply.github.com>
2025-03-27 18:12:36 +0100

    Wip (#1)
    * user wip
    
    * user command

--- src/createApiSign.js
+++ src/createApiSign.js
@@ -1,6 +1,8 @@
 const crypto = require('crypto');
-const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'),"utf-8"));
-module.exports = function createApiSig(params) {
+const fs = require('fs');
+const path = require('path');
+const config = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'config.json'), "utf-8"));
+module.exports = function createApiSign(params) {
     const paramString = Object.keys(params).sort().map(key => key + params[key]).join('');
     const hash = crypto.createHash('md5').update(paramString + config.secret_lastfm).digest('hex');
     return hash;
